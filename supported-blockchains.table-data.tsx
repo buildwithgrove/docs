f189b465fd370fb04c57840d683d3b250ba6954e
Zayne <137804936+zaynepokt@users.noreply.github.com>
2023-10-30 21:13:09 +0100

    Add files via upload

---
+++ supported-blockchains.table-data.tsx
@@ -0,0 +1,317 @@
+import React from "react";
+
+export const supportedBlockchains = [
+    {
+        "name": "Arbitrum One",
+        "portalApiPrefix": "arbitrum-one",
+        "relayChainID": "0066",
+        "evmChainID": "42161"
+    },
+    {
+        "name": "AVAX",
+        "portalApiPrefix": "avax-mainnet",
+        "relayChainID": "0003",
+        "evmChainID": "43114"
+    },
+    {
+        "name": "Base Mainnet",
+        "portalApiPrefix": "base-mainnet",
+        "relayChainID": "0079",
+        "evmChainID": "8453"
+    },
+    {
+        "name": "Base Testnet",
+        "portalApiPrefix": "base-testnet",
+        "relayChainID": "0080",
+        "evmChainID": "84531"
+    },
+    {
+        "name": "BOBA Mainnet",
+        "portalApiPrefix": "boba-mainnet",
+        "relayChainID": "0048",
+        "evmChainID": "288"
+    },
+    {
+        "name": "Binance Smart Chain",
+        "portalApiPrefix": "bsc-mainnet",
+        "relayChainID": "0004",
+        "evmChainID": "56"
+    },
+    {
+        "name": "Binance Smart Chain (Archival)",
+        "portalApiPrefix": "bsc-archival",
+        "relayChainID": "0010",
+        "evmChainID": "56"
+    },
+    {
+        "name": "Celo Mainnet",
+        "portalApiPrefix": "celo-mainnet",
+        "relayChainID": "0065",
+        "evmChainID": "42220"
+    },
+    {
+        "name": "DFKchain Subnet",
+        "portalApiPrefix": "avax-dfk",
+        "relayChainID": "03DF",
+        "evmChainID": "53935"
+    },
+    {
+        "name": "Dogechain Mainnet",
+        "portalApiPrefix": "dogechain-mainnet",
+        "relayChainID": "0059",
+        "evmChainID": "2000"
+    },
+    {
+        "name": "Ethereum Mainnet",
+        "portalApiPrefix": "eth-mainnet",
+        "relayChainID": "0021",
+        "evmChainID": "0001"
+    },
+    {
+        "name": "Ethereum Mainnet Archival",
+        "portalApiPrefix": "eth-archival",
+        "relayChainID": "0022",
+        "evmChainID": "0001"
+    },
+    {
+        "name": "Evmos Mainnet",
+        "portalApiPrefix": "evmos-mainnet",
+        "relayChainID": "0046",
+        "evmChainID": "9001"
+    },
+    {
+        "name": "Fantom Mainnet",
+        "portalApiPrefix": "fantom-mainnet",
+        "relayChainID": "0049",
+        "evmChainID": "250"
+    },
+    {
+        "name": "Fuse",
+        "portalApiPrefix": "fuse-mainnet",
+        "relayChainID": "0005",
+        "evmChainID": "122"
+    },
+    {
+        "name": "Fuse Archival",
+        "portalApiPrefix": "fuse-archival",
+        "relayChainID": "000A",
+        "evmChainID": "122"
+    },
+    {
+        "name": "Gnosis Chain Mainnet",
+        "portalApiPrefix": "gnosischain-mainnet",
+        "relayChainID": "0027",
+        "evmChainID": "100"
+    },
+    {
+        "name": "Gnosis Chain Archival",
+        "portalApiPrefix": "gnosischain-archival",
+        "relayChainID": "000C",
+        "evmChainID": "100"
+    },
+    {
+        "name": "Harmony Shard 0",
+        "portalApiPrefix": "harmony-0",
+        "relayChainID": "0040",
+        "evmChainID": "1666600000"
+    },
+    {
+        "name": "IoTeX Mainnet",
+        "portalApiPrefix": "iotex-mainnet",
+        "relayChainID": "0044",
+        "evmChainID": "4689"
+    },
+    {
+        "name": "Kava Mainnet",
+        "portalApiPrefix": "kava-mainnet",
+        "relayChainID": "0071",
+        "evmChainID": "2222"
+    },
+    {
+        "name": "Kava Mainnet Archival",
+        "portalApiPrefix": "kava-mainnet-archival",
+        "relayChainID": "0072",
+        "evmChainID": "2222"
+    },
+    {
+        "name": "Klaytn Mainnet",
+        "portalApiPrefix": "klaytn-mainnet",
+        "relayChainID": "0056",
+        "evmChainID": "8217"
+    },
+    {
+        "name": "Meter Mainnet",
+        "portalApiPrefix": "meter-mainnet",
+        "relayChainID": "0057",
+        "evmChainID": "82"
+    },
+    {
+        "name": "Metis Mainnet",
+        "portalApiPrefix": "metis-mainnet",
+        "relayChainID": "0058",
+        "evmChainID": "1088"
+    },
+    {
+        "name": "Moonbeam Mainnet",
+        "portalApiPrefix": "moonbeam-mainnet",
+        "relayChainID": "0050",
+        "evmChainID": "1284"
+    },
+    {
+        "name": "Moonriver Mainnet",
+        "portalApiPrefix": "moonriver-mainnet",
+        "relayChainID": "0051",
+        "evmChainID": "1285"
+    },
+    {
+        "name": "NEAR Mainnet",
+        "portalApiPrefix": "near-mainnet",
+        "relayChainID": "0052",
+        "evmChainID": ""
+    },
+    {
+        "name": "OKC Mainnet",
+        "portalApiPrefix": "oKc-mainnet",
+        "relayChainID": "0047",
+        "evmChainID": "66"
+    },
+    {
+        "name": "Oasys Mainnet",
+        "portalApiPrefix": "oasys-mainnet",
+        "relayChainID": "0070",
+        "evmChainID": "248"
+    },
+    {
+        "name": "Oasys Mainnet Archival",
+        "portalApiPrefix": "oasys-mainnet-archival",
+        "relayChainID": "0069",
+        "evmChainID": "248"
+    },
+    {
+        "name": "Optimism Mainnet",
+        "portalApiPrefix": "optimism-mainnet",
+        "relayChainID": "0053",
+        "evmChainID": "10"
+    },
+    {
+        "name": "Osmosis Mainnet",
+        "portalApiPrefix": "osmosis-mainnet",
+        "relayChainID": "0054",
+        "evmChainID": ""
+    },
+    {
+        "name": "Pocket Network Mainnet",
+        "portalApiPrefix": "mainnet",
+        "relayChainID": "0001",
+        "evmChainID": ""
+    },
+    {
+        "name": "Polygon Matic",
+        "portalApiPrefix": "poly-mainnet",
+        "relayChainID": "0009",
+        "evmChainID": "137"
+    },
+    {
+        "name": "Polygon Matic Archival",
+        "portalApiPrefix": "poly-archival",
+        "relayChainID": "000B",
+        "evmChainID": "137"
+    },
+    {
+        "name": "Polygon zkEVM Mainnet",
+        "portalApiPrefix": "polygon-zkevm-mainnet",
+        "relayChainID": "0074",
+        "evmChainID": "1101"
+    },
+    {
+        "name": "Scroll Mainnet",
+        "portalApiPrefix": "scroll-mainnet",
+        "relayChainID": "0082",
+        "evmChainID": "534352"
+    },
+    {
+        "name": "Solana",
+        "portalApiPrefix": "solana-mainnet",
+        "relayChainID": "0006",
+        "evmChainID": ""
+    },
+    {
+        "name": "Sui Mainnet",
+        "portalApiPrefix": "sui-mainnet",
+        "relayChainID": "0076",
+        "evmChainID": ""
+    }
+]
+
+export const supportedTestnetRelayChains = [
+    {
+        "name": "Ethereum Goerli",
+        "portalApiPrefix": "eth-goerli",
+        "relayChainID": "0026",
+        "evmChainID": "005"
+    },
+    {
+        "name": "Ethereum Archival",
+        "portalApiPrefix": "goerli-archival",
+        "relayChainID": "0063",
+        "evmChainID": "005"
+    },
+    {
+        "name": "Ethereum Rinkeby",
+        "portalApiPrefix": "rinkeby",
+        "relayChainID": "0022",
+        "evmChainID": "004"
+    },
+    {
+        "name": "Ethereum Holešky",
+        "portalApiPrefix": "holesky-fullnode-testnet",
+        "relayChainID": "0081",
+        "evmChainID": "17000"
+    },
+    {
+        "name": "Polygon Mumbai",
+        "portalApiPrefix": "polygon-mumbai",
+        "relayChainID": "000F",
+        "evmChainID": "80001"
+    },
+    {
+        "name": "Pocket Network Testnet",
+        "portalApiPrefix": "",
+        "relayChainID": "0002",
+        "evmChainID": ""
+    },
+    {
+        "name": "Scroll Testnet",
+        "portalApiPrefix": "scroll-testnet-alpha",
+        "relayChainID": "0075",
+        "evmChainID": "534353"
+    },
+]
+
+// ----------------------------------------------------------------------------
+// DataTable column definition
+// ----------------------------------------------------------------------------
+export const columns = [
+    {
+        Header: "Name",
+        accessor: "name",
+        className: "pokt-data-table header left",
+    },
+    {
+        Header: "Portal API Prefix",
+        accessor: "portalApiPrefix",
+        className: "pokt-data-table header left",
+    },
+    {
+        Header: "RelayChainID",
+        accessor: "relayChainID",
+        className: "pokt-data-table header left",
+        // disableSortBy: true,
+        // Filter: ""
+    },
+    {
+        Header: "EVM Chain ID",
+        accessor: "evmChainID",
+        className: "pokt-data-table header left",
+    },
+];
